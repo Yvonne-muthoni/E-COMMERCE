40f42352b19eb52c42ab57be6e719680fc998e21
hydrogenbond68 <hydrogenbond68@gmail.com>
2024-07-10 22:15:36 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import LoginPage from './components/LoginPage';
+import ProfilePage from './components/ProfilePage';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/profile" component={ProfilePage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
