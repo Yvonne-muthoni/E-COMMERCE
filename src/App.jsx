045104b28ca3c3c7e51e739640cd2456213d0347
imranabdisalan7 <agmraan@gmail.com>
2024-07-10 22:18:21 +0300

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
-function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/cart">Cart</Link>
+          <Link to="/checkout">Checkout</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
