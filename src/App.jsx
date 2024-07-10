e176e9057ad2a3cfb5dffacab1bd74364b322a9f
Kristian <originalkristiann@mail.com>
2024-07-10 21:12:54 +0300

    add Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Products from './components/Products'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Products />
+    </>
   )
 }
 
