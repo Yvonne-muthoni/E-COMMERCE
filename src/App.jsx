2ba9d29d87ac61486af4dc4ba0c1b720e1d21d68
imranabdisalan7 <agmraan@gmail.com>
2024-07-11 09:21:12 +0300

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Cart from './components/Cart';
-import Checkout from './components/Checkout';
+
 
 const App = () => {
   return (
@@ -10,12 +10,12 @@ const App = () => {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/cart">Cart</Link>
-          <Link to="/checkout">Checkout</Link>
+         
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
+        
         </Routes>
       </div>
     </Router>
